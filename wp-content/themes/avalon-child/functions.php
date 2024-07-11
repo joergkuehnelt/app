4f5462e0ce4c0c3ea9f2233bb5f3df113bd07f56
joergkuehnelt <112785585+joergkuehnelt@users.noreply.github.com>
2024-07-11 18:10:17 +0200

    child

--- wp-content/themes/avalon-child/functions.php
+++ wp-content/themes/avalon-child/functions.php
@@ -1,6 +1,6 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    $parenthandle = 'avalon'; // This is 'avalon' for the Avalon theme.
+    $parenthandle = 'avalon-style'; // This is 'avalon-style' for the Avalon theme.
     $theme = wp_get_theme();
     wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
         array(), // if the parent theme code has a dependency, copy it to here
